7fdac27b63e798b83ca7f2e628e442c43f7ccde8
guptaamit05 <gupta.05.amit@gmail.com>
2025-02-26 16:02:52 +0530

    created getall connections of logged user API.

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,6 +9,7 @@ const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express();
 
+const USER_SAVED_DATA = "firstName lastName gender";
 // GET ALL THE PENDING CONNECTION REQUEST FOR THE LOGGED USER.
 userRouter.get("/api/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -16,11 +17,42 @@ userRouter.get("/api/user/requests/received", userAuth, async (req, res) => {
     const data = await ConnectionRequest.find({
       toUserId: loggedUser._id,
       status: "interested",
-    }).populate("fromUserId", ["firstName", "lastName", "gender"]);
+    }).populate("fromUserId", USER_SAVED_DATA);
     if (!data.length) {
       return res
         .status(404)
         .json({ status: 404, message: "No connection request found." });
+    }
+    res.status(200).json({
+      status: 200,
+      message: "all connection request fetched",
+      data: data,
+    });
+  } catch (e) {
+    res.status(400).send({ status: 400, message: e.message });
+  }
+});
+
+userRouter.get("/api/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedUser = req.user;
+    const allConnections = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedUser._id, status: "accepted" },
+        { fromUserId: loggedUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAVED_DATA);
+
+    const data = allConnections.map((row) => {
+      if (row.fromUserId._id.toString() === loggedUser._id.toString()) {
+        return row;
+      }
+      return row.fromUserId;
+    });
+    if (!data.length) {
+      return res
+        .status(404)
+        .json({ status: 404, message: "No connections found." });
     }
     res
       .status(200)
