45de88f92176783a79cb20be07e912de6c620d5b
guptaamit05 <gupta.05.amit@gmail.com>
2025-02-26 17:37:45 +0530

    created feed API. and also added pagination in feed API.

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,13 +3,17 @@
 //GET  /user/requests/received    ( interested status)
 //GET  /user/feed
 
+//  skip() && limit()  two function for pagination.
+
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express();
 
-const USER_SAVED_DATA = "firstName lastName gender";
+const USER_SAVED_DATA =
+  "firstName lastName email gender photoUrl age about skills";
 // GET ALL THE PENDING CONNECTION REQUEST FOR THE LOGGED USER.
 userRouter.get("/api/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -33,6 +37,7 @@ userRouter.get("/api/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
+// GET ALL CONNECTIONS OF ACCEPTED STATUS.
 userRouter.get("/api/user/connections", userAuth, async (req, res) => {
   try {
     const loggedUser = req.user;
@@ -57,6 +62,46 @@ userRouter.get("/api/user/connections", userAuth, async (req, res) => {
     res
       .status(200)
       .json({ status: 200, message: "all connection fetched", data: data });
+  } catch (e) {
+    res.status(400).send({ status: 400, message: e.message });
+  }
+});
+
+// FEED API.
+userRouter.get("/api/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+    // get all users whose I am not marked as: ignored, interested, rejected, and if also has connections with other user(don't show )
+    // user shouldn't see his own card, his connection (already connected user), already ignored, or already sent the connection request to user. these users we don't have to send in API.
+
+    const data = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedUser._id }, { toUserId: loggedUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed = new Set();
+    data.forEach((row) => {
+      hideUserFromFeed.add(row.fromUserId.toString());
+      hideUserFromFeed.add(row.toUserId.toString());
+    });
+    // console.log(hideUserFromFeed);
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedUser._id } },
+      ],
+    })
+      .select(USER_SAVED_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    //  skip formula :  (page-1)*limit
+    res.status(200).json({ status: 200, message: "all feed fetched", users });
   } catch (e) {
     res.status(400).send({ status: 400, message: e.message });
   }
